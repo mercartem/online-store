89cc0741f6dec5f8de9d002e549fc0bbd4c11dae
Artemiy Gudz <mercartem@mail.ru>
2022-12-15 23:33:14 +0300

    feat: add data.js

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,6 +1,5 @@
-import data from './constans/data';
-const { products } = data;
+import products from './constans/data';
 
 const a = document.querySelector('.aaa') as HTMLElement;
 
-a.innerHTML = `<img src=${products[0].thumbnail}/>`;
+a.innerHTML = `<img src=${products[5].thumbnail}/><div>${products[5].title}</div>`;
