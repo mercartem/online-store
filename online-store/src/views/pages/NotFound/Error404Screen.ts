1e727683cc99e002c4242b8fe1826dcc5b1201ae
Artemiy Gudz <mercartem@mail.ru>
2022-12-16 22:48:26 +0300

    fix: fix style error 404

--- online-store/src/views/pages/NotFound/Error404Screen.ts
+++ online-store/src/views/pages/NotFound/Error404Screen.ts
@@ -1,16 +1,14 @@
 import { Screen } from '../../../constans/types/interfaces';
-import { error } from '../../../constans/images';
 
 class Error404Screen implements Screen {
   render() {
     return `
     <div class="error">
       <div class="error__info">
-        <h1 class="error__title font_XXL">Something went wrong...</h1>
+        <h1 class="error__title font_XXL">Error 404!<br>Something went wrong...</h1>
         <h5 class="error__text font_S">This page does not exist. Go back to the catalog.</h5>
-        <button class="btn btn_L btn_primary">Go to catalog</button>
+        <button class="btn btn_L btn_primary" href="/#/">Go to catalog</button>
       </div>
-      <img src="${error}" alt="Error"/>
     <div>`;
   }
 }
