e2c66b88a83b0ad513bb9f749f65a02e20a51cdd
Artemiy Gudz <mercartem@mail.ru>
2022-12-19 13:39:32 +0300

    fix: fix styles

--- online-store/src/views/pages/Main/HomeScreen.ts
+++ online-store/src/views/pages/Main/HomeScreen.ts
@@ -5,27 +5,29 @@ import { Screen } from '../../../constans/types/interfaces';
 class HomeScreen implements Screen {
   render() {
     return `
-    <ul class="products">
-      ${products
-        .map(
-          (product) => `
-      <li>
-        <div class="product">
-          <h5 class="product__name font_S">
-            <a href="/#/product/${product.id}">${product.title}</a>
-          </h5>
-          <a href="/#/product/${product.id}">
-            <img src="${product.thumbnail}" alt="${product.title}" />
-          </a>
-          <h4 class="product__price font_M">${product.price} ₽</h4>
-          <button class="btn btn_M btn_primary">ADD TO CART</button>
-          <div class="product__stock font_XXS">In stock: ${product.stock}</div>
-        </div>
-      </li>
-      `
-        )
-        .join('\n')}
-    </ul>
+    <div class="page__container main__container container">
+      <ul class="products">
+        ${products
+          .map(
+            (product) => `
+        <li>
+          <div class="product">
+            <h5 class="product__name font_S">
+              <a href="/#/product/${product.id}">${product.title}</a>
+            </h5>
+            <a href="/#/product/${product.id}">
+              <img src="${product.thumbnail}" alt="${product.title}" />
+            </a>
+            <h4 class="product__price font_M">${product.price} ₽</h4>
+            <button class="btn btn_M btn_primary">ADD TO CART</button>
+            <div class="product__stock font_XXS">In stock: ${product.stock}</div>
+          </div>
+        </li>
+        `
+          )
+          .join('\n')}
+      </ul>
+    </div>
     `;
   }
 }
