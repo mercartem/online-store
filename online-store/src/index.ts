3000b9766667143f48d85ebc66173d99ba3fc35d
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2022-12-24 19:43:08 +0700

    refactor: test modal window

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -5,10 +5,16 @@ import { parseRequestUrl } from './constans/utils';
 import HomeScreen from './views/pages/Main/HomeScreen';
 import ProductScreen from './views/pages/Product/ProductScreen';
 import Error404Screen from './views/pages/NotFound/Error404Screen';
+import Modal from './views/components/Modal/Modal';
 
 const homeScreen: HomeScreen = new HomeScreen();
 const productScreen: ProductScreen = new ProductScreen();
 const error404Screen: Error404Screen = new Error404Screen();
+const modal: Modal = new Modal();
+
+// const main = document.querySelector('.page') as HTMLElement;
+// main.innerHTML = modal.render();
+// modal.afterRender();
 
 const routes: Record<string, Screen> = {
   '/': homeScreen,
