41ce36cc3da0ce0bd5f87f1bf6fe62e5518602c5
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2022-12-29 13:32:01 +0700

    fix: code readability Error404Screen

--- online-store/src/views/pages/NotFound/Error404Screen.ts
+++ online-store/src/views/pages/NotFound/Error404Screen.ts
@@ -1,23 +1,22 @@
 import { Screen } from '../../../constans/types/interfaces';
-import { rerender } from '../../../constans/utils';
-import header from '../../components/Header/header';
 
 class Error404Screen implements Screen {
-  public afterRender(): void {
-    (document.querySelector('.error__btn') as HTMLDivElement).addEventListener('click', () => {
-      console.log('click');
+  afterRender() {
+    // Переход в каталог при клике на кнопку
+    const buttonError = document.querySelector('.error__btn') as HTMLButtonElement;
+    buttonError.addEventListener('click', () => {
       document.location.hash = `/`;
     });
   }
 
-  public render(): string {
+  render() {
     return `
     <div class="page__container main__container container">
       <div class="error">
         <div class="error__info">
           <h1 class="error__title font_XXL">Error 404!<br>Something went wrong...</h1>
           <h5 class="error__text font_S">This page does not exist. Go back to the catalog.</h5>
-          <button class="btn btn_L btn_primary error__btn">Go to catalog</button>
+          <button class="error__btn btn btn_L btn_primary">Go to catalog</button>
         </div>
       <div>
     </div>`;
